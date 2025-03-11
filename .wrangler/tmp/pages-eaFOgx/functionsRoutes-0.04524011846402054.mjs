682ffdad949c436f20acdcb86101773bf14b5e43
meeeeeooooowwwwwww <generalflynn17@gmail.com>
2025-03-12 10:09:49 +1300

    Backup branch created on 20250312 with all current changes

---
+++ .wrangler/tmp/pages-eaFOgx/functionsRoutes-0.04524011846402054.mjs
@@ -0,0 +1,11 @@
+import { onRequest as __biz_js_onRequest } from "C:\\Users\\David\\Documents\\GitHub\\nataliegwinters\\functions\\biz.js"
+
+export const routes = [
+    {
+      routePath: "/biz",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [__biz_js_onRequest],
+    },
+  ]
